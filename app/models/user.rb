871d5614eb60e0921f2143e0a1d289a431f7beac
takamine <takamine.yuta@gmail.com>
2025-12-30 17:36:02 +0900

    ActiveStorage導入とデフォルト画像追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  has_many :rooms, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true
 end
